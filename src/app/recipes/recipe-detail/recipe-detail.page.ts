8162db2e09502315fd5f1d8c1c83fb7ad9e83e14
kalph <kalpupadhyay@gmail.com>
2021-05-30 14:34:25 +0530

    finished design and added favorite icon

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -10,21 +11,60 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeDetailPage implements OnInit {
 
-
+  isFavourite = false;
   loadedRecipe: Recipe;
 
-  constructor(private activatedRoute: ActivatedRoute, private route: Router, private recipeService: RecipeService) { }
+  constructor(private activatedRoute: ActivatedRoute,
+    private route: Router, private recipeService: RecipeService,
+    private alertCtrl: AlertController) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
       if (!paramMap.has('recipeId')) {
-
         return;
       }
       const recipeID = paramMap.get('recipeId');
       this.loadedRecipe = this.recipeService.getRecipe(recipeID);
-
     });
+  }
+
+  addToFav() {
+    if (this.isFavourite) {
+      this.alertCtrl.create({
+        header: 'Remove from favourite?', buttons: [
+          {
+            text: 'No',
+            role: 'cancel'
+          },
+          {
+            text: 'Yes',
+            handler: () => {
+              this.isFavourite = false;
+            }
+          }
+        ]
+      }).then(alertEl => {
+        alertEl.present();
+      });
+    } else {
+      this.alertCtrl.create({
+        header: 'Add this to favourite?', buttons: [
+          {
+            text: 'No',
+            role: 'cancel'
+          },
+          {
+            text: 'Yes',
+            handler: () => {
+              this.isFavourite = true;
+            }
+          }
+        ]
+      }).then(alertEl => {
+        alertEl.present();
+      });
+    }
+
   }
 
 }
